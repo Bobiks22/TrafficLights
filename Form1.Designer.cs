606b47a952c3da570d6453144c878e55d4d0ec6f
Bobiks <Bobiks@MATISS-GETAC>
2020-06-06 18:34:46 +0300

    added functionality

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TrafficLights
 {
-    partial class Form1
+    partial class TrafficLights
     {
         /// <summary>
         /// Required designer variable.
@@ -28,60 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.RedLight = new System.Windows.Forms.PictureBox();
+            this.YellowLight = new System.Windows.Forms.PictureBox();
+            this.GreenLight = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RedLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowLight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenLight)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // RedLight
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(43, 29);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.RedLight.Location = new System.Drawing.Point(43, 29);
+            this.RedLight.Name = "RedLight";
+            this.RedLight.Size = new System.Drawing.Size(100, 100);
+            this.RedLight.TabIndex = 0;
+            this.RedLight.TabStop = false;
             // 
-            // pictureBox2
+            // YellowLight
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(43, 163);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.YellowLight.Location = new System.Drawing.Point(43, 163);
+            this.YellowLight.Name = "YellowLight";
+            this.YellowLight.Size = new System.Drawing.Size(100, 100);
+            this.YellowLight.TabIndex = 1;
+            this.YellowLight.TabStop = false;
             // 
-            // pictureBox3
+            // GreenLight
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(43, 292);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.GreenLight.Location = new System.Drawing.Point(43, 292);
+            this.GreenLight.Name = "GreenLight";
+            this.GreenLight.Size = new System.Drawing.Size(100, 100);
+            this.GreenLight.TabIndex = 2;
+            this.GreenLight.TabStop = false;
             // 
-            // Form1
+            // TrafficLights
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(183, 450);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Name = "Form1";
+            this.Controls.Add(this.GreenLight);
+            this.Controls.Add(this.YellowLight);
+            this.Controls.Add(this.RedLight);
+            this.Name = "TrafficLights";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RedLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.YellowLight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GreenLight)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox RedLight;
+        private System.Windows.Forms.PictureBox YellowLight;
+        private System.Windows.Forms.PictureBox GreenLight;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,112 @@ using System.Windows.Forms;
 
 namespace TrafficLights
 {
-    public partial class Form1 : Form
+    public partial class TrafficLights : Form
     {
-        public Form1()
+        private Timer timerColorSwitch = null;
+        private Timer timerBlink = null;
+        private PictureBox picB = null;
+        private Color color = Color.Gray;
+        private int seconds = 0;
+
+        public TrafficLights()
         {
             InitializeComponent();
+            InitiaizeTrafficLights();
+            InitializeTimerColorSwitch();
+            InitializeTimerBlink();
+        }
+
+        private void InitiaizeTrafficLights()
+        {
+
+            // set shape
+            RedLight.Region = Round(RedLight);
+            YellowLight.Region = Round(YellowLight);
+            GreenLight.Region = Round(GreenLight);
+            // Set Backcolor
+            RedLight.BackColor = Color.Gray;
+            YellowLight.BackColor = Color.Gray;
+            GreenLight.BackColor = Color.Gray;
+
+            
+        }
+
+        private void InitializeTimerColorSwitch()
+        {
+            timerColorSwitch = new Timer
+            {
+                Interval = 1000
+            };
+            timerColorSwitch.Tick += new EventHandler(TimerColorSwitch_Tick);
+            timerColorSwitch.Start();
+
+        }
+
+        private void InitializeTimerBlink()
+        {
+            timerBlink = new Timer
+            {
+                Interval = 200
+            };
+            timerBlink.Tick += new EventHandler(TimerBlink_Tick);
+        }
+
+        private void TimerColorSwitch_Tick(object sender, EventArgs e)
+        {
+            switch (seconds)
+            {
+                case 0:
+                    RedLight.BackColor = Color.Red;
+                    break;
+                case 3:
+                    YellowLight.BackColor = Color.Yellow;
+
+                    picB = RedLight;
+                    color = Color.Red;
+                    timerBlink.Start();
+                    break;
+                case 5:
+                    timerBlink.Stop();
+                    YellowLight.BackColor = Color.Gray;
+                    GreenLight.BackColor = Color.Green;
+                    break;
+                case 6:
+                    picB = GreenLight;
+                    color = Color.Green;
+                    timerBlink.Start();
+                    break;
+                case 8:
+                    YellowLight.BackColor = Color.Yellow;
+                    GreenLight.BackColor = Color.Gray;
+                    timerBlink.Stop();
+                    break;
+                case 10:
+                    YellowLight.BackColor = Color.Gray;
+                    RedLight.BackColor = Color.Red;
+                    seconds = -1;
+                    break;
+            }
+            seconds++;
+        }
+
+        private void TimerBlink_Tick(object sender, EventArgs e)
+        {
+            if (picB.BackColor == color)
+            {
+                picB.BackColor = Color.Gray;
+            }
+            else
+            {
+                picB.BackColor = color;
+            }
+        }
+
+        private Region Round(PictureBox picB)
+        {
+            System.Drawing.Drawing2D.GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddEllipse(0, 0, picB.Width, picB.Height);
+            return new Region(gp);
         }
     }
 }
